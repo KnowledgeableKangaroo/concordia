b38c5ff646afe958d99b93585504782a6dd7dbe2
Rosie Storey <rstorey@loc.gov>
2018-08-21 16:18:03 -0400

    Changes needed to create a docker image that uses RDS DB host

--- concordia/settings_template.py
+++ concordia/settings_template.py
@@ -61,7 +61,7 @@ DATABASES = {
         "NAME": "concordia",
         "USER": "concordia",
         "PASSWORD": "$(POSTGRESQL_PW)",
-        "HOST": "db",
+        "HOST": "$(POSTGRESQL_HOST)",
         "PORT": "5432",
     }
 }
